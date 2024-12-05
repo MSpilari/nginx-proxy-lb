89860222b5cf637d8945df6ca9691b14a294fb8a
MSpilari <mb.spilari@hotmail.com>
2024-12-05 17:15:09 -0300

    feat: add circuit breaker and fallback method

--- src/main/java/dev/mspilari/spring_app_server/controllers/HelloWorld.java
+++ src/main/java/dev/mspilari/spring_app_server/controllers/HelloWorld.java
@@ -2,19 +2,32 @@ package dev.mspilari.spring_app_server.controllers;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorld {
 
     @GetMapping()
+    @CircuitBreaker(name = "helloWorldCircuitBreaker", fallbackMethod = "fallbackHelloWorld")
     public ResponseEntity<Map<String, String>> helloWorld() {
 
+        if (Math.random() < 0.5) {
+            throw new RuntimeException("Service Unavailable");
+        }
         return ResponseEntity.ok().body(Map.of("message", "Hello world from: " + System.getenv("SERVER_NAME")));
+    }
+
+    public ResponseEntity<Map<String, String>> fallbackHelloWorld(Throwable t) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(Map.of("message",
+                        "Fallback response: " + System.getenv("SERVER_NAME") + " service temporarily unavailable."));
     }
 
 }
